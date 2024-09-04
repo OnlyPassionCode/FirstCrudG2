bb2728bee210b5cc15e214fcb32e87c36ddf4160
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-09-04 16:21:34 +0200

    menu

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $articles = $em->getRepository(Article::class)->findAll();
         return $this->render('home/index.html.twig', [
             'title' => 'Homepage',
+            'articles' => $articles
         ]);
     }
 }
